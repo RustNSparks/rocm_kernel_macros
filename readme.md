8c8c1226a5dddf19924810430b033cae747a071a
PTFOPlayer <patryk041103@gmail.com>
2025-05-11 11:14:16 +0200

    feat: Rework macro to allow  multiple functions and additional structures

---
+++ readme.md
@@ -0,0 +1,10 @@
+# rocm_kernel_macros
+
+Crate for generating subprojects with kernel source written in rust
+
+## Requirements
+
+1. rust nightly
+2. rust target: amdgcn-amd-amdhsa
+3. rust-src
+4. ROCM 6.0 or newer
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,24 @@ use proc_macro::TokenStream;
 use quote::quote;
 use std::process::Command;
 use std::{fs, path::Path};
-use syn::{ItemFn, parse_macro_input};
+use syn::{File, Item, parse_macro_input};
 
 #[proc_macro]
 pub fn amdgpu_kernel(input: TokenStream) -> TokenStream {
-    let kernel_fn = parse_macro_input!(input as ItemFn);
-    let fn_name = kernel_fn.sig.ident.to_string();
+    let file = parse_macro_input!(input as File);
 
-    let kernel_body = quote!(#kernel_fn).to_string();
+    let mut kernel_body = String::new();
+    let mut kernel_names = Vec::new();
+
+    for item in file.items {
+        if let Item::Fn(ref func) = item {
+            let name = func.sig.ident.to_string();
+            kernel_names.push(name);
+        }
+        // Append the code of all items to be written into lib.rs
+        kernel_body += &quote!(#item).to_string();
+        kernel_body += "\n\n";
+    }
 
     let preamble = r#"
 #![no_std]
@@ -41,46 +51,42 @@ unsafe extern "C" {
 
     let full_source = format!("{preamble}\n\n{kernel_body}");
 
-    let kernel_dir = Path::new("kernel_sources").join(&fn_name);
+    let kernel_dir_name = kernel_names.join("_");
+
+    generate_enviorment(&kernel_dir_name, &full_source);
+
+    let binary_path_str = build(&kernel_dir_name);
+
+    let output = quote! {
+        pub const KERNEL_BINARY_PATH: &str = #binary_path_str;
+    };
+
+    output.into()
+}
+
+fn generate_enviorment(name: &str, src: &str) {
+    // create kernel source path
+    let kernel_dir = Path::new("kernel_sources").join(name);
     fs::create_dir_all(kernel_dir.join("src")).unwrap();
 
-    let cargo_toml = format!(
-        r#"
-[package]
-name = "kernels"
-version = "0.1.0"
-edition = "2024"
+    // write source
+    fs::write(kernel_dir.join("src/lib.rs"), src).unwrap();
 
-[dependencies]
-
-
-[lib]
-crate-type = ["cdylib"]
-
-[profile.dev]
-lto = true
-[profile.release]
-lto = true
-"#
-    );
-
-    fs::write(kernel_dir.join("Cargo.toml"), cargo_toml).expect("Failed to write Cargo.toml");
-
+    // generate and write cargo config
     let cargo_config_dir = kernel_dir.join(".cargo");
     fs::create_dir_all(&cargo_config_dir).expect("Failed to create .cargo directory");
 
-    let config_toml = r#"
-[build]
-target = "amdgcn-amd-amdhsa"
-rustflags = ["-Ctarget-cpu=gfx1102"]
-
-[unstable]
-build-std = ["core"]
-    "#;
+    let config_toml = include_str!("cargo_config_template.toml");
     fs::write(cargo_config_dir.join("config.toml"), config_toml)
         .expect("Failed to write .cargo/config.toml");
 
-    fs::write(kernel_dir.join("src/lib.rs"), full_source).expect("Failed to write lib.rs");
+    // write cargo toml
+    let cargo_toml = include_str!("cargo_template.toml");
+    fs::write(kernel_dir.join("Cargo.toml"), cargo_toml).expect("Failed to write Cargo.toml");
+}
+
+fn build(fn_name: &str) -> String {
+    // building with cargo
 
     let build_command = format!(
         "cd {}/kernel_sources/{}; cargo build --release; cd ./../../;",
@@ -105,11 +111,5 @@ build-std = ["core"]
         .join("release");
     let binary_path = target_dir.join("kernels.elf");
 
-    let binary_path_str = binary_path.display().to_string();
-
-    let output = quote! {
-        pub const KERNEL_BINARY_PATH: &str = #binary_path_str;
-    };
-
-    output.into()
+    binary_path.display().to_string()
 }
